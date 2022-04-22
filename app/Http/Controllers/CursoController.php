fe9a29d3530abe92c85e462cec749d8bfd1ff6f4
DanielRC123 <danroman111@misena.edu.co>
2022-04-22 17:37:12 -0500

    Quedamos en el método store del controlador CursoController. Falta el index

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Curso;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('cursos.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cursos.create');
+    }
+
+    /**
+     * Almacena un nuevo registro creado.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // all: me trae toda la informacion almacenada en request
+        //return $request->all();
+        //creamos una instancia del modelo para manipular la tabla Curso
+        $cursito = new Curso();
+        // a travez de ka unstancia $cursito llamo al campo nombre de la
+        // tabla curso y le asigno el valor que viene del request
+        $cursito->nombre = $request->input('nombre');
+        //hago lo mismo con el campo de descripcion
+        $cursito->descripcion = $request->input('descripcion');
+        $cursito->save();
+        return 'Curso creado exitosamente';
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('titulo','Listado Cursos')
+@section('contenido')
+<br>
+<p> Aquí encontrará el listado de cursos</p>
+@endsection
